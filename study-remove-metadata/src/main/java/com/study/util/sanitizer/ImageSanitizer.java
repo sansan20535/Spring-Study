cd6dfb71e5ae37b005e22772e305743d04a61c21
sansan20535 <20san20@daum.net>
2025-12-16 16:21:06 +0900

    [Study] : 요청 파일 사이즈 크기 증가 및 webp는 ffmpeg 사용하도록 수정

--- study-remove-metadata/src/main/java/com/study/util/sanitizer/ImageSanitizer.java
+++ study-remove-metadata/src/main/java/com/study/util/sanitizer/ImageSanitizer.java
@@ -1,5 +1,6 @@
 package com.study.util.sanitizer;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.imageio.ImageIO;
@@ -7,10 +8,22 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class ImageSanitizer {
+
+    private final FfmpegRunner ffmpegRunner;
+
     public byte[] sanitize(byte[] input, String lowerName) {
+
+        if (lowerName.endsWith(".webp")) {
+            return sanitizeWebpWithFfmpeg(input);
+        }
+
         String format = detectFormat(lowerName);
 
         try (ByteArrayInputStream in = new ByteArrayInputStream(input)) {
@@ -26,6 +39,38 @@ public class ImageSanitizer {
             throw new RuntimeException("이미지 메타데이터 제거 처리 오류", e);
         }
     }
+
+    private byte[] sanitizeWebpWithFfmpeg(byte[] input) {
+        Path inFile = null;
+        Path outFile = null;
+
+        try {
+            inFile = Files.createTempFile("webp-in-", ".webp");
+            outFile = Files.createTempFile("webp-out-", ".webp");
+            Files.write(inFile, input);
+
+            ffmpegRunner.run(List.of(
+                    "-y",
+                    "-i", inFile.toString(),
+                    "-map_metadata", "-1",
+                    outFile.toString()
+            ));
+
+            return Files.readAllBytes(outFile);
+        } catch (Exception e) {
+            throw new RuntimeException("WEBP 메타데이터 제거 실패", e);
+        } finally {
+            if (inFile != null) try {
+                Files.deleteIfExists(inFile);
+            } catch (IOException ignored) {
+            }
+            if (outFile != null) try {
+                Files.deleteIfExists(outFile);
+            } catch (IOException ignored) {
+            }
+        }
+    }
+
 
     private String detectFormat(String lowerName) {
         if (lowerName.endsWith(".png")) {
