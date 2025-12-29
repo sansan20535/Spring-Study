4bb714dd51a2b72f5cf72005013e5ef61bdc4487
sansan20535 <20san20@daum.net>
2025-12-30 08:38:02 +0900

    [STUDY] 파파고 번역 API

---
+++ study-translation/src/main/java/com/translation/TranslationStudyApplication.java
@@ -0,0 +1,11 @@
+package com.translation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationStudyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TranslationStudyApplication.class, args);
+    }
+}
